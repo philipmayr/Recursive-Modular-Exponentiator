129648736c3018c7586ad7ee90e76743efd7e2e7
Phil Samuel <philipmayr@outlook.com>
2024-11-04 10:40:03 -0600

    Create file.c

---
+++ file.c
@@ -0,0 +1,29 @@
+// Recursive Modular Exponentiator
+
+#include <stdio.h>
+
+long long exponentiate_modularly(int base, int index, int modulus)
+{
+    if (index == 0) return 1 % modulus;
+    
+    long long power = exponentiate_modularly(base, index >> 1, modulus);
+    
+    power = (power * power) % modulus;
+    
+    if (index & 1) power = (power * base) % modulus;
+    
+    return power;
+}
+
+int main()
+{
+    int base = 2;
+    int index = 2;
+    int modulus = 2;
+    
+    long long modulated_power = exponentiate_modularly(base, index, modulus);
+    
+    printf("%d^%d ≡ %lld (mod %d)", base, index, modulated_power, modulus);
+    
+    return 0;
+}
